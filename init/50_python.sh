59748a4d1ea1ae8485ff3537d6d72837cff247a2
Christian Sauer <sauerc@gmail.com>
2016-06-26 17:05:40 -0400

    improve pyenv init

--- init/50_python.sh
+++ init/50_python.sh
@@ -1,11 +1,13 @@
+
 py2=$(pyenv install --list | grep -E "\s+2.\d+\." | tail -1)
 py3=$(pyenv install --list | grep -E "\s+3.\d+\." | tail -1)
 
 echo py2 is $py2
 echo py3 is $py3
 
-pyenv install $py3
-pyenv install $py2
+(pyenv versions | grep $py3 > /dev/null 2>&1) || pyenv install $py3
+(pyenv versions | grep $py2 > /dev/null 2>&1) || pyenv install $py2
+
 pyenv rehash
 pyenv global $py3 $py2
 
@@ -13,4 +15,4 @@ pipcmd=$(pyenv which pip)
 
 $pipcmd install --upgrade pip
 
-$pipcmd install virtualenvwrapper
+$pipcmd install --upgrade virtualenvwrapper
