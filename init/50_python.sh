027f7470e4507cd533ad0fe3aaac4913b15fd59a
Christian Sauer <sauerc@gmail.com>
2016-06-28 11:01:34 -0400

    better still control over python versions

--- init/50_python.sh
+++ init/50_python.sh
@@ -1,6 +1,6 @@
 
-py2=$(pyenv install --list | grep -E "\s+2.7\." | tail -1)
-py3=$(pyenv install --list | grep -E "\s+3.5\." | tail -1)
+py2=$(pyenv install --list | grep -ve "[a-zA-Z]" | grep -E "\s+2.\d+\." | tail -1)
+py3=$(pyenv install --list | grep -ve "[a-zA-Z]" | grep -E "\s+3.\d+\." | tail -1)
 
 echo py2 is $py2
 echo py3 is $py3
