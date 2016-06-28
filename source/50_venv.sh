0e93c94bffd49774832bb0352158747deb19ed4a
Christian Sauer <sauerc@gmail.com>
2016-06-28 19:21:55 -0400

    venv.sh not needed, all handled in 50_python now

--- source/50_venv.sh
+++
@@ -1,6 +0,0 @@
-
-[[ -e /usr/local/bin/virtualenvwrapper.sh ]] || return 1
-
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-export PIP_VIRTUALENV_BASE=$WORKON_HOME
