b19430b7b8c01dd7733cf48b0e397511a24e4fe5
Christian Sauer <sauerc@gmail.com>
2017-10-24 23:00:38 -0400

    shortcut for installing new python versions

--- source/50_python.sh
+++ source/50_python.sh
@@ -16,3 +16,10 @@ alias allpep8='find . -name "*.py" -print0 | xargs -0 pep8'
 alias pyclean='find -E . \( \( -type f -regex ".*\.py(c|o|\.class)$" \) -or \( -type d -name "__pycache__" \) \) -delete'
 
 export PIPENV_DEFAULT_PYTHON_VERSION=3.6
+
+function pyenv_install() {
+	[[ ! "$1" ]] && echo "Specify a python version" && return 1
+	CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix zlib)/include -I$(brew --prefix sqlite)/include" \
+	LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix zlib)/lib -L$(brew --prefix sqlite)/lib" \
+	pyenv install -f $1
+}
