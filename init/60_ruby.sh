0ca7cf9130eeec1587f916be4cb8a78fe38e8a9b
Christian Sauer <sauerc@gmail.com>
2018-02-25 16:08:08 -0500

    ruby is rbenv, not pyenv

--- init/60_ruby.sh
+++ init/60_ruby.sh
@@ -5,7 +5,7 @@ rb2=$(rbenv install --list | grep -ve "[a-zA-Z]" | grep -E "\s+2\.\d+\." | tail 
 
 echo rb2 is $rb2
 
-(rbenv versions | grep $rb2 > /dev/null 2>&1) || pyenv install $rb2
+(rbenv versions | grep $rb2 > /dev/null 2>&1) || rbenv install $rb2
 
 rbenv rehash
 rbenv global $rb2
