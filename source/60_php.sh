97e4fec0d94b6296b00a244d0d4ea7f431da1326
Christian Sauer <sauerc@gmail.com>
2021-04-28 22:00:34 -0400

    php path change in homebrew

--- source/60_php.sh
+++ source/60_php.sh
@@ -1,3 +1,3 @@
 PATH=~/.composer/vendor/bin:"$(path_remove ~/.composer/vendor/*/bin)"
-PATH="/usr/local/Cellar/php@7.4/7.4.13_1/bin:$PATH"
+PATH="/usr/local/opt/php@7.4/bin:/usr/local/opt/php@7.4/sbin:$PATH"
 export PATH
