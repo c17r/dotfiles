18f38ad7d02ca881ae7d470f027b0b4bbe5d5634
Christian Sauer <148640+c17r@users.noreply.github.com>
2021-07-28 13:27:26 -0400

    Get virtualenvwrapper working again

--- source/60_python.sh
+++ source/60_python.sh
@@ -4,6 +4,7 @@ if which pyenv > /dev/null; then
 	export PYENV_ROOT="$HOME/.pyenv"
 	export PATH="$PYENV_ROOT/bin:$(path_remove $PYENV_ROOT/bin)"
 	eval "$(pyenv init --path)"
+	eval "$(pyenv init -)"
 	pyenv virtualenvwrapper
 fi
 
