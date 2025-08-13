58a2ace6a2a6a4632adbaa870c5dc920aee7046f
Christian Sauer <148640+c17r@users.noreply.github.com>
2025-08-13 19:51:19 -0400

    Update 05_brew.sh

--- source/05_brew.sh
+++ source/05_brew.sh
@@ -6,8 +6,15 @@ export HOMEBREW_NO_ENV_HINTS=1
 
 export HOMEBREW_BIN=/opt/homebrew/bin
 export PATH="$HOMEBREW_BIN:$(path_remove $HOMEBREW_BIN)"
+
 export HOMEBREW_SBIN=/opt/homebrew/sbin
 export PATH="$HOMEBREW_SBIN:$(path_remove $HOMEBREW_SBIN)"
+
+export HOMEBREW_CPATH="/opt/homebrew/include"
+export CPATH="$HOMEBREW_CPATH:$(path_remove $HOMEBREW_CPATH)"
+
+export HOMEBREW_LIBRARY_PATH="/opt/homebrew/lib"
+export LIBRARY_PATH="$HOMEBREW_LIBRARY_PATH:$(path_remove $HOMEBREW_LIBRARY_PATH)"
 
 function brew() {
 	/usr/bin/env PATH="$(path_remove $(pyenv root)/shims)" /opt/homebrew/bin/brew "$@"
